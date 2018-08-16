c7ba2fab193b385078c0479655445e0b6b6183c4
Raffi Baghdasaryan <raffii90@gmail.com>
2018-08-16 22:58:59 +0200

    Workaround for unity hanging on second play

---
+++ Assets/Scripts/CustomMenuItems.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CustomMenuItems {
+    [MenuItem("Tools/Reset bidding plugin initialization")]
+    private static void ClearBiddingPluginInitialization()
+    {
+        PlayerPrefs.DeleteKey("IsBiddingPluginInitialized");
+    }
+}
